bd43fadf7d87f3922d2250f3f00f320a4769d86d
Isak Starlander <isakstarlander@gmail.com>
2018-07-05 19:29:51 +0200

    Testing base page with some bootstrap

--- home/urls.py
+++ home/urls.py
@@ -6,6 +6,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', views.HomePageView.as_view(), name='home'),
     path('signup/', views.SignUp.as_view(), name='signup'),
 ]
--- home/views.py
+++ home/views.py
@@ -10,3 +10,6 @@ class SignUp(generic.CreateView):
     form_class = CustomUserCreationForm 
     success_url = reverse_lazy('login') 
     template_name = 'signup.html'
+
+class HomePageView(generic.TemplateView):
+    template_name = 'home.html'

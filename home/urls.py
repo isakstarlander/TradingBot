227d5057dc26c0801aaa7fc9c1ca8475cb0b1d37
Isak Starlander <isakstarlander@gmail.com>
2018-07-05 18:15:08 +0200

    added necessary paths and changed signup form to accept email

---
+++ home/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path 
+
+from django.views.generic.base import TemplateView
+
+from . import views
+
+
+urlpatterns = [
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('signup/', views.SignUp.as_view(), name='signup'),
+]
